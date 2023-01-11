5e67454cb203753f1f4795d616c410ba12062f94
Nikita Lutsai <105123083+nikitalutsai@users.noreply.github.com>
2023-01-11 22:13:42 +0100

    Add description, image and links

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-col1, col2 = st.columns(2)
+col1, col2= st.columns(2)
 
 with col1:
     st.image("images/photo.png")
@@ -18,12 +18,20 @@ content2 = """
 Below you can find some of the apps I have built in Python. Feel free to contact me!
 """
 st.caption(content2)
+
 df = pandas.read_csv("data.csv", sep=";")
-col3, col4 = st.columns(2)
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write("[Source Code](https://pythonhow.com)")
