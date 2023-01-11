1d177974725b1b4ae9c87a4d2f09bc7c1ba04995
Nikita Lutsai <105123083+nikitalutsai@users.noreply.github.com>
2023-01-11 20:46:51 +0100

    Create columns, image and description

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Nikita Lutsai")
+    content = """
+    Hi, i am Nikita! I am a Python programmer. I graduated in Associate's Degree in Marketing and International Trade. Beside these studies I realized that my true passion is programming. I quited my regular job in order to be able to study as much as possible and get a job where I can really enjoy what I am doing. 
+    """
+    st.info(content)
