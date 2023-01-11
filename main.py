3d63d66687a3d36f201e00caff9a8b022a9c966f
Nikita Lutsai <105123083+nikitalutsai@users.noreply.github.com>
2023-01-11 21:49:50 +0100

    Render the titles in two columns

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import pandas
 import streamlit as st
+
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
@@ -16,4 +18,12 @@ content2 = """
 Below you can find some of the apps I have built in Python. Feel free to contact me!
 """
 st.caption(content2)
+df = pandas.read_csv("data.csv", sep=";")
+col3, col4 = st.columns(2)
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
 
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
