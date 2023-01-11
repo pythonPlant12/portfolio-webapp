118963668d90a56447011cf9b66bbe2913562c2b
Nikita Lutsai <105123083+nikitalutsai@users.noreply.github.com>
2023-01-11 20:59:46 +0100

    Add description on the new columns.

--- main.py
+++ main.py
@@ -12,3 +12,8 @@ with col2:
     Hi, i am Nikita! I am a Python programmer. I graduated in Associate's Degree in Marketing and International Trade. Beside these studies I realized that my true passion is programming. I quited my regular job in order to be able to study as much as possible and get a job where I can really enjoy what I am doing. 
     """
     st.info(content)
+content2 = """
+Below you can find some of the apps I have built in Python. Feel free to contact me!
+"""
+st.caption(content2)
+
