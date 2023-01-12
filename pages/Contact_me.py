332d01b855c4c303197e8d5674afe6db082d8849
Nikita Lutsai <105123083+nikitalutsai@users.noreply.github.com>
2023-01-12 16:19:17 +0100

    Send emails using the form

--- pages/Contact_me.py
+++ pages/Contact_me.py
@@ -1,11 +1,18 @@
 import streamlit as st
+from send_email import send_email
 
 st.set_page_config(layout="wide")
 st.header("Contact me")
 
 with st.form(key="email_form"):
     user_email = st.text_input("Enter your email")
-    message = st.text_area("Your message")
+    row_message = st.text_area("Your message")
+    message = f"""\
+Subject: New email from {user_email}
+
+From: {user_email}
+{row_message}"""
     button = st.form_submit_button()
     if button:
-        print("I was pressed")
+        send_email(message)
+        st.info("Your email was sent successfully")
--- send_email.py
+++ send_email.py
@@ -1,21 +1,15 @@
 import smtplib, ssl
 
 
-def send_email():
+def send_email(message):
     host = "smtp.gmail.com"
     port = 465
     username = "testpythonplant@gmail.com"
     password = "rmtztiqhliswezbi"
     receiver = "testpythonplant@gmail.com"
-    message = """\
-Subject: Hi!
-How are you?
-Bye!
-"""
+
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, username, message)
 
-
-send_email()
