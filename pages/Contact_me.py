c44c9cb42be1b32bfd7ef0a9e64c944c48a1de62
Nikita Lutsai <105123083+nikitalutsai@users.noreply.github.com>
2023-01-12 19:40:29 +0100

    Setting up environment variables, adding selectBox for topic using pandas

--- pages/Contact_me.py
+++ pages/Contact_me.py
@@ -1,18 +1,25 @@
 import streamlit as st
-from send_email import send_email
+import pandas
+from send_email import send_mail
 
+topic_options = []
 st.set_page_config(layout="wide")
 st.header("Contact me")
+df = pandas.read_csv("/Users/nikitalutsai/PycharmProjects/portfolio-app/pages/topics.csv")
+for index, item in df.iterrows():
+    topic_options.append(item["topic"])
 
 with st.form(key="email_form"):
     user_email = st.text_input("Enter your email")
+    topic = st.selectbox("What's your purpose of contacting me", topic_options)
     row_message = st.text_area("Your message")
     message = f"""\
 Subject: New email from {user_email}
 
 From: {user_email}
+Topic: {topic}
 {row_message}"""
     button = st.form_submit_button()
     if button:
-        send_email(message)
+        send_mail(message)
         st.info("Your email was sent successfully")
--- send_email.py
+++ send_email.py
@@ -1,15 +1,17 @@
 import smtplib, ssl
+import os
 
 
-def send_email(message):
+def send_mail(message):
     host = "smtp.gmail.com"
     port = 465
     username = "testpythonplant@gmail.com"
-    password = "rmtztiqhliswezbi"
-    receiver = "testpythonplant@gmail.com"
+    password = os.getenv("PASSWORD")
 
+    receiver = "testpythonplant@gmail.com"
     context = ssl.create_default_context()
+
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
-        server.sendmail(username, username, message)
+        server.sendmail(username, receiver, message)
 
